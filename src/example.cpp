f8f23cdd5c479fb465072e2dfb160a90267fd694
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-11-17 23:33:19 +0100

    Implement Example::print(std::array<double, n>) method

--- src/example.cpp
+++ src/example.cpp
@@ -29,5 +29,11 @@ struct Example{
         std::cout << std::endl;
     }
 
-    void print(const std::array<swzcpu::f64, 8>& vec);
+    // -*-
+    void print(const std::array<swzcpu::f64, 8>& vec){
+        for(auto item: vec){
+            std::cout << std::dec << item << " ";
+        }
+        std::cout << std::endl;
+    }
 };
